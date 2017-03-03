8d2a44d4d74f04541eb3ccd934fb243849cd80dc
Edmund <447ght@gmail.com>
2017-03-03 11:09:26 -0500

    completed algorithms
    the dictionary is not very efficient

---
+++ dict-clock/dict-clock.swift
@@ -0,0 +1,9 @@
+//
+//  dict-clock.swift
+//  YouGiveLoveABadName
+//
+//  Created by Edmund Holderbaum on 3/3/17.
+//  Copyright © 2017 Flatiron School. All rights reserved.
+//
+
+import Foundation
